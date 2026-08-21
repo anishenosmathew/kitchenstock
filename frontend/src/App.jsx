345d5e4bb78c9dede63b6137fbc2e725cf1f50b1
Enos Mathew <330013@cognizant.com>
2026-08-21 13:36:08 +0530

    Added keyinsights

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import Login from './pages/Login';
 import Signup from './pages/Signup';
